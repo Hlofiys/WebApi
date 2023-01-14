6ca8de86f33a30d3bbc16f7cabd97eb1809fa69e
Hlofiys <hlofiys@gmail.com>
2023-01-14 22:49:58 +0300

    Auth fix

--- Program.cs
+++ Program.cs
@@ -20,13 +20,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
-        Description = """Standard Authorization header using the Bearer scheme. Example: "bearer {token}" """,
+        Description = """Авторизация тут для теста. Пример ввода: "{token}" """,
         In = ParameterLocation.Header,
-        Name = "Authorization",
+        Name = "x-access-token",
         Type = SecuritySchemeType.ApiKey
     });
+
+     c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+          }
+      });
 
     c.OperationFilter<SecurityRequirementsOperationFilter>();
 });
