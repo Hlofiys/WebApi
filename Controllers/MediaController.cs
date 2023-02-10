1f1cc4fb8847b77a9f0cf58dd0e5ac0669da1ec7
Hlofiys <hlofiys@gmail.com>
2023-02-10 23:10:11 +0300

    media 3

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -18,12 +18,30 @@ namespace WebApi.Controllers
         public ActionResult GetFile(string CategoryName)
         {
             string FileName = Request.Query["name"]!;
-            string FilePath = $"/images/{CategoryName}/{FileName}";
-            FilePath = Path.Combine(Directory.GetCurrentDirectory(), FilePath);
-            if (System.IO.File.Exists(FilePath))
-                return PhysicalFile(FilePath, "image/png");
+            if (OperatingSystem.IsWindows())
+            {
+                string FilePath = $"images/{CategoryName}/{FileName}";
+                FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, FilePath);
+                Console.WriteLine(FilePath);
+                if (System.IO.File.Exists(FilePath))
+                    return PhysicalFile(FilePath, "image/png");
+                else
+                    return NotFound();
+            }
+            else if (OperatingSystem.IsLinux())
+            {
+                string FilePath = $"images/{CategoryName}/{FileName}";
+                FilePath = Path.Combine("/home/hlofiys/dotnet/WebApi/bin/Debug/net7.0/", FilePath);
+                if (System.IO.File.Exists(FilePath))
+                    return PhysicalFile(FilePath, "image/png");
+                else
+                    return NotFound();
+            }
             else
+            {
                 return NotFound();
+            }
+            
         }
     }
 }
