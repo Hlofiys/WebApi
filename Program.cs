a8751f7ba53de17f2f5615d93ab08704c3576f4b
Hlofiys <hlofiys@gmail.com>
2023-03-14 22:32:22 +0300

    test

--- Program.cs
+++ Program.cs
@@ -94,7 +94,7 @@ app.MapGet("api/addItems",  async (DataContext db) => {
   await startUp.AddTypes().ContinueWith(async i => await startUp.AddItems());
 });
 
-app.UseCors(options => options.WithOrigins("http://localhost:3000", "https://kirikkostya.github.io").AllowCredentials().AllowAnyHeader().AllowAnyMethod());
+app.UseCors(options => options.WithOrigins("http://localhost:3000", "https://kirikkostya.github.io/").AllowCredentials().AllowAnyHeader().AllowAnyMethod());
 app.UseAuthentication();
 app.UseAuthorization();
 
