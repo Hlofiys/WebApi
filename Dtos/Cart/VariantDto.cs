a20b15ec6535945c87842b9e062dbc9a606fc27c
hlofiys <hlofiys@gmail.com>
2023-05-19 19:27:03 +0300

    fix

--- Dtos/Cart/VariantDto.cs
+++ Dtos/Cart/VariantDto.cs
@@ -4,7 +4,7 @@ namespace WebApi.Dtos.Cart
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public List<string> Description { get; set; } = new List<string>();
+        public string Description { get; set; } = string.Empty;
         public int? Price { get; set; } = null;
         public List<string>? Icon { get; set; } = null;
         public int? ItemId { get; set; } = null;
